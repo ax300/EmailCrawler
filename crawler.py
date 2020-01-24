02507d9cc472a8f7d7e04df781e4969748c142f2
ax300 <andre.cavicchiolli@usp.br>
2020-01-24 14:15:46 -0300

    preenche planilha

--- crawler.py
+++ crawler.py
@@ -1,11 +1,13 @@
 # Importing libraries
 import imaplib, email, getpass, html2text, re, datetime, os.path, xlwt, xlrd
+from xlutils.copy import copy as xl_copy
 
 
 #user = 'andre.cavicchiolli@usp.br'
 imap_url = 'imap.gmail.com'
 user = ''
 password = ''
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
   
 #pega informacoes de login e as guarda num txt  
 def get_pass():
@@ -80,22 +82,34 @@ def get_emails(result_bytes):
                 #print("===========================================================================================================================")
     return msgs 
 
-#cria planilha e a preenche com as formulas
-def create_sheet(file_name):
-    f = open("base.xls", "w")
+#cria aba na planilha
+def create_sheet(sheet_name):
     # Insere regras e headers da planilha
+    book1 = xlrd.open_workbook('base.xlsx',  formatting_info=True)
+    book = xl_copy(book1)
+    sheet = book.add_sheet(sheet_name)
+    #print(book1.sheet_names())
+    #fill_sheet(book1)
+    fill_sheet(sheet, book)
+
+#cria arquivo
+def create_file(sheet_name):
     book = xlwt.Workbook()
+    sheet = book.add_sheet(sheet_name)
+    fill_sheet(sheet,book)
+
+#preenche planilha
+def fill_sheet(sheet, book):
     #campos e formulas
-    sheet = book.add_sheet(file_name)
-    sheet.write(0, 1, 'Janeiro')
-    sheet.write(0, 2, 'Fevereiro')
-    sheet.write(0, 3, 'Março')
-    sheet.write(0, 4, 'Abril')
-    sheet.write(0, 5, 'Maio')
-    sheet.write(0, 6, 'Junho')
-    sheet.write(0, 7, 'Julho')
-    sheet.write(0, 8, 'agosto')
-    sheet.write(0, 9, 'Setembro')
+    sheet.write(0, 1,  'Janeiro')
+    sheet.write(0, 2,  'Fevereiro')
+    sheet.write(0, 3,  'Março')
+    sheet.write(0, 4,  'Abril')
+    sheet.write(0, 5,  'Maio')
+    sheet.write(0, 6,  'Junho')
+    sheet.write(0, 7,  'Julho')
+    sheet.write(0, 8,  'agosto')
+    sheet.write(0, 9,  'Setembro')
     sheet.write(0, 10, 'Outubro')
     sheet.write(0, 11, 'Novembro')
     sheet.write(0, 12, 'Dezembro')
@@ -103,52 +117,66 @@ def create_sheet(file_name):
     sheet.write(4,0,'Total Mensal')
     sheet.write(5,0,'Total')
     #Totais Mensais
-    sheet.write(4, 1,  '=SOMA(B2:B4)')
-    sheet.write(4, 2,  '=SOMA(C2:C4)')
-    sheet.write(4, 3,  '=SOMA(D2:D4)')
-    sheet.write(4, 4,  '=SOMA(F2:F4)')
-    sheet.write(4, 5,  '=SOMA(G2:G4)')
-    sheet.write(4, 6,  '=SOMA(H2:H4)')
-    sheet.write(4, 7,  '=SOMA(I2:I4)')
-    sheet.write(4, 8,  '=SOMA(J2:J4)')
-    sheet.write(4, 9,  '=SOMA(K2:K4)')
-    sheet.write(4, 10, '=SOMA(L2:L4)')
-    sheet.write(4, 11, '=SOMA(M2:M4)')
-    sheet.write(4, 12, '=SOMA(N2:N4)')
+    sheet.write(4, 1,  xlwt.Formula('SUM(B2:B4)'))
+    sheet.write(4, 2,  xlwt.Formula('SUM(C2:C4)'))
+    sheet.write(4, 3,  xlwt.Formula('SUM(D2:D4)'))
+    sheet.write(4, 4,  xlwt.Formula('SUM(E2:E4)'))
+    sheet.write(4, 5,  xlwt.Formula('SUM(F2:F4)'))
+    sheet.write(4, 6,  xlwt.Formula('SUM(G2:G4)'))
+    sheet.write(4, 7,  xlwt.Formula('SUM(H2:H4)'))
+    sheet.write(4, 8,  xlwt.Formula('SUM(I2:I4)'))
+    sheet.write(4, 9,  xlwt.Formula('SUM(J2:J4)'))
+    sheet.write(4, 10, xlwt.Formula('SUM(K2:K4)'))
+    sheet.write(4, 11, xlwt.Formula('SUM(L2:L4)'))
+    sheet.write(4, 12, xlwt.Formula('SUM(M2:M4)'))
     #Totais
-    sheet.write(5, 1,  '=SOMA(B5;A6)')
-    sheet.write(5, 2,  '=SOMA(C5;B6)')
-    sheet.write(5, 3,  '=SOMA(D5;C6)')
-    sheet.write(5, 4,  '=SOMA(F5;D6)')
-    sheet.write(5, 5,  '=SOMA(G5;E6)')
-    sheet.write(5, 6,  '=SOMA(H5;F6)')
-    sheet.write(5, 7,  '=SOMA(I5;G6)')
-    sheet.write(5, 8,  '=SOMA(J5;H6)')
-    sheet.write(5, 9,  '=SOMA(K5;I6)')
-    sheet.write(5, 10, '=SOMA(L5;J6)')
-    sheet.write(5, 11, '=SOMA(M5;K6)')
-    sheet.write(5, 12, '=SOMA(N5;L6)')
-    book.save("base.xls")
+    sheet.write(5, 1,  xlwt.Formula('SUM(B5;A6)'))
+    sheet.write(5, 2,  xlwt.Formula('SUM(C5;B6)'))
+    sheet.write(5, 3,  xlwt.Formula('SUM(D5;C6)'))
+    sheet.write(5, 4,  xlwt.Formula('SUM(E5;D6)'))
+    sheet.write(5, 5,  xlwt.Formula('SUM(F5;E6)'))
+    sheet.write(5, 6,  xlwt.Formula('SUM(G5;F6)'))
+    sheet.write(5, 7,  xlwt.Formula('SUM(H5;G6)'))
+    sheet.write(5, 8,  xlwt.Formula('SUM(I5;H6)'))
+    sheet.write(5, 9,  xlwt.Formula('SUM(J5;I6)'))
+    sheet.write(5, 10, xlwt.Formula('SUM(K5;J6)'))
+    sheet.write(5, 11, xlwt.Formula('SUM(L5;K6)'))
+    sheet.write(5, 12, xlwt.Formula('SUM(M5;L6)'))
+    book.save("base.xlsx")
 
 # this is done to make SSL connnection with GMAIL 
 con = imaplib.IMAP4_SSL(imap_url)  
 # Pega informacoes de login ou mantem as atuais
 login_info()
 # logging the user in 
-con.login(user,password)
+    #con.login(user,password)
   
 # calling function to check for email under this label 
 
-con.select('Inbox')  
+    #con.select('Inbox')  
   
+
  # fetching emails from this user "tu**h*****1@gmail.com" 
-msgs = get_emails(search('FROM', 'todomundo@nubank.com.br', con)) 
+
+
+    #msgs = get_emails(search('FROM', 'todomundo@nubank.com.br', con)) 
+
+
 #msgs = get_emails(search(None,'all' con))   
 
 # verifica se a planilha com os dados ja existe
 year = datetime.datetime.today().year
-file_name = str(year)
-if (os.path.isfile("base.xls") == False):
+#year = '2026'
+sheet_name = str(year)
+'''
+for name in xlrd.open_workbook("base.xlsx").sheet_names():
+    print(name)
+
+print(sheet_name in xlrd.open_workbook("base.xlsx").sheet_names())
+'''
+if (os.path.isfile("base.xlsx") == False):
     print('cria')
-    create_sheet(file_name)
-    
+    create_file(sheet_name)
+elif ((sheet_name in xlrd.open_workbook("base.xlsx").sheet_names())== False):
+    print('cria sheet')
+    create_sheet(sheet_name)
